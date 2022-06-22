2fbe3f3264308bbb06a3026796b038d3cd767bbd
aalroy <98309142+aalroy@users.noreply.github.com>
2022-06-22 17:25:53 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -108,3 +108,25 @@ resource "aws_s3_bucket" "logs" {
     Environment = local.resource_prefix.value
   }
 }
+
+resource "aws_s3_bucket" "logs2222" {
+  bucket = "${local.resource_prefix.value}-logs"
+  region = "us-west-2"
+  acl    = "log-delivery-write"
+  versioning {
+    enabled = true
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm     = "aws:kms"
+        kms_master_key_id = "${aws_kms_key.logs_key.arn}"
+      }
+    }
+  }
+  force_destroy = true
+  tags = {
+    Name        = "${local.resource_prefix.value}-logs"
+    Environment = local.resource_prefix.value
+  }
+}
